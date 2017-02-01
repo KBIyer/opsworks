ce95b517b0a9492d48d6f02c18e71274563ab21d
KBIyer <KeshIyer@gmail.com>
2017-02-01 16:39:50 -0500

    updated pw

--- OpsWorksDemo/recipes/default.rb
+++ OpsWorksDemo/recipes/default.rb
@@ -2,7 +2,10 @@ Chef::Log.info("********** Hello, World! **********")
 script "Run a script" do
   interpreter "bash"
   code <<-EOH
-    sudo dpkg --add-architecture i386; sudo apt-get update;sudo apt-get install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux tmux lib32gcc1 libstdc++6 libstdc++6:i386
+    sudo dpkg --add-architecture i386; sudo apt-get update;sudo apt-get install -y mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux tmux lib32gcc1 libstdc++6 libstdc++6:i386
+    sudo adduser q3server --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+    echo "q3server:test25" | sudo chpasswd
+    su - q3server
     wget https://gameservermanagers.com/dl/q3server
     chmod +x /q3server
     ./q3server install
