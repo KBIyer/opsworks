52ef6729769228bd6e6493807864986cdcf2993c
KBIyer <KeshIyer@gmail.com>
2017-02-02 14:34:11 -0500

    test301

--- OpsWorksDemo/recipes/default.rb
+++ OpsWorksDemo/recipes/default.rb
@@ -4,8 +4,9 @@ script "Run a script" do
   code <<-EOH
     sudo dpkg --add-architecture i386; sudo apt-get update;sudo apt-get install -y mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux tmux lib32gcc1 libstdc++6 libstdc++6:i386
     sudo adduser q3server --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-    echo "q3server:test25" | sudo chpasswd
-    su -c "wget https://gameservermanagers.com/dl/q3server ; chmod +x q3server; sh /home/q3server auto-install" -m "q3server"
+    wget https://gameservermanagers.com/dl/q3server
+    chmod +x q3server
+    sh /home/q3server auto-install
   EOH
 end
 Chef::Log.info("********** Bye, World! **********")
